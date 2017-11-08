123d8a42d6434bbec69a9d455f7efeb4723618bf
Giuseppe Futia <giuseppe.futia@gmail.com>
2017-11-08 12:44:20 +0100

    Update script for jar generation

--- create_jar.sh
+++ create_jar.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-original_file="./target/rodi-2.0.0-SNAPSHOT-jar-with-dependencies.jar"
-file="./RODI_benchmark.jar"
+original_file="./target/rodi-2.0.0-SNAPSHOT.jar"
+target_file="./RODI_benchmark.jar"
 
 echo "Remove old jar file"
 
-[ -e $file ] && rm $file
+[ -e $target_file ] && rm $target_file
 
 mvn clean
 mvn package
 
 echo "Move the new jar file in the root directory"
 
-mv $original_file ./$file
+mv $original_file ./$target_file
